713714150a5fa7f625c2729e22de5a08d333ce9b
Katia Bahrin <katia.b1101@gmail.com>
2023-11-05 10:54:36 +0100

    fix

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,24 +1,23 @@
 import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_4LFtrKZ60t1sSxyWQIMaxwmXv2zlodstUsaDoW06kaYtPaMPvYTC90vu15TR1WnN";
+
+axios.defaults.headers.common["x-api-key"] = "live_4LFtrKZ60t1sSxyWQIMaxwmXv2zlodstUsaDoW06kaYtPaMPvYTC90vu15TR1WnN"; 
 
 export function fetchBreeds() {
     return axios.get("https://api.thecatapi.com/v1/breeds")
         .then(response => {
-            // console.log(response.data);
-            return response.data;  
+            return response.data;
         })
-       .catch(error => {
-    console.log(error);
-  });
-       
-};
+        .catch(error => {
+            throw error;
+        });
+}
 
 export function fetchCatByBreed(breedId) {
     return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-        .then(result => {
-            return result;
+        .then(response => {
+            return response.data;
         })
         .catch(error => {
-            console.log(error);
-        })
-};
+            throw error;
+        });
+}
