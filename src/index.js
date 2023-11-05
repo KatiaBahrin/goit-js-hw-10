03263d5a3cb1ef70d3645f29221dfecb546bd58e
Katia Bahrin <katia.b1101@gmail.com>
2023-11-05 10:46:48 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "../node_modules/slim-select/dist/slimselect.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
 import { makeMarkupforSelect } from "./services/makeMarkupforSelect";
-import {  makeCatCardMarkup } from "./services/makeCatCardMarkup";
+import { makeCatCardMarkup } from "./services/makeCatCardMarkup";
 
 const refs = {
     catInfo: document.querySelector('.cat-info'),
@@ -15,29 +15,40 @@ const refs = {
 refs.selectEl.classList.add("visually-hidden");
 refs.error.classList.add("visually-hidden");
 
-    fetchBreeds()
-        .then(data => {
-            const markup = makeMarkupforSelect(data);
-            refs.selectEl.innerHTML = markup;
-             new SlimSelect({
-             select: '#selectElement'
-            })
+// Функція для показу лоадера та приховання вибору породи
+function showLoader() {
+    refs.loader.classList.remove("visually-hidden");
+    refs.selectEl.classList.add("visually-hidden");
+    refs.error.classList.add("visually-hidden");
+}
 
-            refs.loader.classList.add("visually-hidden");
-            refs.selectEl.classList.remove("visually-hidden");;
+// Функція для приховання лоадера та відображення вибору породи
+function hideLoader() {
+    refs.loader.classList.add("visually-hidden");
+    refs.selectEl.classList.remove("visually-hidden");
+}
+
+fetchBreeds()
+    .then(data => {
+        const markup = makeMarkupforSelect(data);
+        refs.selectEl.innerHTML = markup;
+        new SlimSelect({
+            select: '#selectElement'
         })
-        .catch(error => {
-            // console.log(error);
-            Notify.failure(`❌ Oops!, Something went wrong! Try reloading the page!`);
-        });
-    
+
+        hideLoader(); // Приховуємо лоадер після завершення запиту
+    })
+    .catch(error => {
+        Notify.failure(`❌ Oops!, Something went wrong! Try reloading the page!`);
+        hideLoader(); // Приховуємо лоадер при помилці запиту
+    });
 
 refs.selectEl.addEventListener('change', onSelectHandler);
 
 function onSelectHandler(event) {
+    showLoader(); // Показуємо лоадер під час вибору породи
     refs.catInfo.classList.add("visually-hidden");
-    refs.loader.classList.remove("visually-hidden");;
-    refs.error.classList.add("visually-hidden");;
+    refs.error.classList.add("visually-hidden");
 
     const selectedBreedId = event.currentTarget.value;
 
@@ -52,13 +63,12 @@ function onSelectHandler(event) {
             
             const catMarkup = makeCatCardMarkup(name, url, temperament, description);
             refs.catInfo.innerHTML = catMarkup;
-            refs.loader.classList.add("visually-hidden");;
+            hideLoader(); // Приховуємо лоадер після завершення запиту
             refs.catInfo.classList.remove("visually-hidden");
         })
         .catch(error => {
-            // console.log(error);
             Notify.failure(`❌ Oops!, Something went wrong! Try reloading the page!`);
-            refs.loader.classList.add("visually-hidden");;
+            hideLoader(); // Приховуємо лоадер при помилці запиту
             refs.catInfo.classList.add("visually-hidden");
         });   
 }
